eb9a80d5fc5217be31d049ab8c05941566bfec99
evi1Husky <evi1husky0@gmail.com>
2022-11-22 22:45:08 +1000

    pomodoro loop logic using recursive  async await

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Timer {
   #minutes = 0;
   #seconds = 0;
   #initialSeconds = 0;
+  completed = false;
 
   constructor(minutes, seconds) {
     this.#minutes = minutes;
@@ -32,13 +33,21 @@ class Timer {
 
   start() {
     this.#timer();
-    this.#interval = setInterval(() => this.#timer(), 350);
+    this.#interval = setInterval(() => this.#timer(), 450);
+
   }
 
   stop() {
     clearInterval(this.#interval);
     Renderer.updateClockFace(this.#minutes, this.#seconds);
     Renderer.updateTimeBar(this.#initialSeconds, true);
+    this.completed = true;
+  }
+
+  stopButton() {
+    clearInterval(this.#interval);
+    Renderer.updateClockFace(this.#minutes, this.#seconds);
+    this.completed = true;
   }
 }
 
@@ -76,6 +85,18 @@ class Renderer {
       this.#timeBar.style.width = "0%";
     }
   }
+
+  static resetTimeBar() {
+    this.#timeBar.style.width = "100%";
+  }
+
+  static clockFaceColorBreak() {
+    this.#clockFace.style.color = "rgb(157, 207, 83)";
+  }
+
+  static clockFaceColorWork() {
+    this.#clockFace.style.color = "tomato";
+  }
 }
 
 // Main class, contains game loop and logic 
@@ -83,21 +104,105 @@ class Renderer {
 class Pomodoro {
   #timer = Object;
   #startButton = document.querySelector(".start");
-  #workTime = [25, 0];
+  #continueButton = document.querySelector(".continue");
+  #workTime = [0, 2];
+  #shortBreak = [0, 2];
+  #breakTimeOut = [0, 10];
+  #buttonPressed = false;
 
   loop() {
+    this.#continueButton.addEventListener("click", () => {
+      this.#buttonPressed = true;
+      this.#continueButton.disabled = true;
+      this.#continueButton.style.background = "#424752";
+      setTimeout(() => {
+        this.#enableStartButton();
+        Renderer.resetTimeBar();
+        Renderer.clockFaceColorWork();
+        Renderer.updateClockFace(...this.#workTime);
+      }, 3000);
+    });
     Renderer.updateClockFace(...this.#workTime);
     Renderer.updateTabTitle("PomoTime");
-    this.#startButtonEvent();
-  }
-
-  #startButtonEvent() {
+    this.#buttonPressed = true;
     this.#startButton.addEventListener("click", () => {
-      this.#startButton.disabled = true;
-      this.#startButton.style.background = "#424752";
+      this.#disableStartButton();
+      Renderer.resetTimeBar();
+      Renderer.clockFaceColorWork();
+      Renderer.updateClockFace(...this.#workTime);
       this.#timer = new Timer(...this.#workTime);
       this.#timer.start();
+      this.#waitForBreak();
     });
+  }
+
+  #checkIfTimerStopped() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(this.#timer.completed);
+      }, 2000);
+    });
+  }
+  
+  async #waitForBreak() {
+    const hasStopped = await this.#checkIfTimerStopped();
+    if (hasStopped === false) {
+      return this.#waitForBreak();
+    } else {
+      Renderer.updateClockFace(...this.#shortBreak);
+      Renderer.clockFaceColorBreak();
+      this.#timer = new Timer(...this.#shortBreak);
+      Renderer.resetTimeBar();
+      this.#timer.start();
+      this.#breakTime();
+    }
+  }
+
+  async #breakTime() {
+    const hasStopped = await this.#checkIfTimerStopped();
+    if (hasStopped === false) {
+      return this.#breakTime();
+    } else {
+      Renderer.resetTimeBar();
+      Renderer.clockFaceColorWork();
+      this.#timer = new Timer(...this.#breakTimeOut);
+      Renderer.updateClockFace(...this.#breakTimeOut);
+      this.#timer.start();
+      this.#buttonPressed = false;
+      this.#enableContinueButton();
+      this.#waitFromBreak();
+    }
+  }
+
+  async #waitFromBreak() {
+    const hasStopped = await this.#checkIfTimerStopped();
+    if (this.#buttonPressed) {
+      this.#timer.stopButton();
+      return;
+    } else if (hasStopped === false) {
+      return this.#waitFromBreak();
+    } else {
+      return alert("game over");
+    }
+  }
+
+  #disableStartButton() {
+    this.#startButton.disabled = true;
+    this.#startButton.style.background = "#424752";
+  }
+
+  #enableStartButton() {
+    this.#startButton.disabled = false;
+    this.#startButton.style.background = "rgb(157, 207, 83)";
+    this.#startButton.style.display = "block";
+    this.#continueButton.style.display = "none";
+  }
+
+  #enableContinueButton() {
+    this.#continueButton.disabled = false;
+    this.#continueButton.style.background = "rgb(157, 207, 83)";
+    this.#startButton.style.display = "none";
+    this.#continueButton.style.display = "block";
   }
 }
 
